317d3b238a6b1068431c09e7506b1e50d1fefeb4
ryoutakimura <yellow11053@gmail.com>
2023-11-12 09:38:56 +0900

    消費税の計算を行って表示する

--- src/Review01.java
+++ src/Review01.java
@@ -2,8 +2,20 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        int price = 1111; //商品価格
+        int tax = tax(price); //商品の消費税
+        System.out.println(tax(price));
 
     }
 
+    //引数として受け取った商品価格から消費税額を計算して返す
+    public static int tax(int price) {
+        int tax_rate = 10; //消費税率
+        /*
+         * 消費税＝商品価格*消費税率/(100%)
+         * 小数点切り捨て
+         */
+        int tax = price * tax_rate / 100;
+        return tax;
+    }
 }
